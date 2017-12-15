4e0e703bd3c13c185fd8724b2da5a859815b66bb
Marius Adašiūnas <marius.adasiunas@gmail.com>
2017-12-15 16:15:30 +0200

    adding deployement configuration

--- migrations/2_deploy_managers.js
+++ migrations/2_deploy_managers.js
@@ -1,7 +1,31 @@
 var ContractManager = artifacts.require("./ContractManager.sol");
 var ProductManager = artifacts.require("./ProductManager.sol");
+var EventEmmiter = artifacts.require("./EventEmitter.sol");
+
+const abiEncoder = require('ethereumjs-abi');
+const eventEmitterABI = require('../build/contracts/EventEmitter.json');
+const contractManagerABI = require('../build/contracts/ContractManager.json')
 
 module.exports = function(deployer) {
-  	deployer.deploy(ContractManager);
-  	deployer.deploy(ProductManager);
+  	return deployer.deploy(EventEmmiter).then(async() => {
+		const eventEmitter = await EventEmmiter.deployed();
+		const encodeParamsEventEmitter = abiEncoder.rawEncode(['address'], [eventEmitter.address]);
+		
+		console.log('\n\n\nContract EventEmitter address:\n', eventEmitter.address);
+		console.log('ENCODED PARAMS EventEmitter:\n', encodeParamsEventEmitter.toString('hex'));
+		console.log('EventEmitter ABI:\n', JSON.stringify(eventEmitterABI));
+		console.log('\n\n\n');
+
+		await deployer.deploy(ContractManager, eventEmitter.address);
+		const contractManager = await ContractManager.deployed();
+		const encodeParamsContractManager = abiEncoder.rawEncode(['address'], [contractManager.address]);
+		await eventEmitter.addExecutor(contractManager.address);
+		contractManager.setContract("EventEmitter", eventEmitter.address);
+
+		console.log('\n\n\nContractManager address:\n', contractManager.address);
+		console.log('ENCODED PARAMS ContractManager:\n', encodeParamsContractManager.toString('hex'));
+		console.log('ContractManager ABI:\n', JSON.stringify(contractManagerABI));
+		console.log('\n\n\n');
+		
+	});
 };
--- migrations/3_deploy_product_contracts.js
+++
@@ -1,8 +0,0 @@
-//var BatteryProductData = artifacts.require("./insuranceProducts/BatteryProductData.sol");
-//var BatteryProductController = artifacts.require("./insuranceProducts/BatteryProductController.sol");
-var EventEmmiter = artifacts.require("./EventEmitter.sol");
-
-module.exports = function(deployer) {
-  	deployer.deploy(EventEmmiter);
- // 	deployer.deploy(BatteryProductController);
-};
